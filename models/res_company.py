5268814e2fe863dc736ae47938bbaa997e677d9b
iS-AJ <alfredo@isolpa.com>
2024-11-19 15:49:31 -0500

    proper branch and pos code handling

--- models/res_company.py
+++ models/res_company.py
@@ -10,8 +10,16 @@ class ResCompany(models.Model):
         help='Branch code for HKA integration (4 digits)'
     )
 
-    @api.constrains('hka_branch_code')
-    def _check_hka_branch_code(self):
+    hka_pos_code = fields.Char(
+        string='HKA POS Code',
+        default='001',
+        help='Default Point of Sale code for HKA integration (3 digits). Used when POS module is not installed.'
+    )
+
+    @api.constrains('hka_branch_code', 'hka_pos_code')
+    def _check_hka_codes(self):
         for company in self:
             if company.hka_branch_code and (not company.hka_branch_code.isdigit() or len(company.hka_branch_code) != 4):
                 raise ValidationError(_('HKA Branch Code must be exactly 4 digits'))
+            if company.hka_pos_code and (not company.hka_pos_code.isdigit() or len(company.hka_pos_code) != 3):
+                raise ValidationError(_('HKA POS Code must be exactly 3 digits'))
