1ccbc579b08af82e595535c72832cf35ec30c4b5
iS-AJ <alfredo@isolpa.com>
2024-11-21 15:44:59 -0500

    send pos invoiced orders to hka

--- models/pos_order.py
+++ models/pos_order.py
@@ -9,32 +9,22 @@ class PosOrder(models.Model):
     def _generate_pos_order_invoice(self):
         """Override to handle HKA integration when creating invoices from POS orders"""
         # Call super to create the invoice
-        result = super()._generate_pos_order_invoice()
+        moves = super()._generate_pos_order_invoice()
         
-        # If no invoice was created, return early
-        if not result:
-            return result
-            
-        # Get the created invoice(s)
-        if isinstance(result, list):
-            invoice_ids = result
-        else:
-            invoice_ids = [result]
-            
-        # Get the invoice records
-        moves = self.env['account.move'].browse(invoice_ids)
+        if not moves:
+            return moves
+
+        # Get the actual invoice record
+        if not isinstance(moves, models.Model):
+            invoice = self.account_move_id
+            if invoice:
+                try:
+                    invoice._send_to_hka()
+                except Exception as e:
+                    _logger.warning(
+                        'Invoice %s created but failed to send to HKA: %s',
+                        invoice.name or 'Unknown',
+                        str(e)
+                    )
         
-        # Send each created invoice to HKA
-        for move in moves:
-            try:
-                if hasattr(move, '_send_to_hka'):
-                    move._send_to_hka()
-            except Exception as e:
-                # Log error but don't stop the process
-                _logger.warning(
-                    'Invoice %s created but failed to send to HKA: %s',
-                    move.display_name if hasattr(move, 'display_name') else 'Unknown',
-                    str(e)
-                )
-        
-        return result
+        return moves
