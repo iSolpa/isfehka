84feb779522495db1b25477a9a8664f5f161ef9f
iS-AJ <alfredo@isolpa.com>
2024-11-26 22:03:52 -0500

    consumidor final cliente support

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Panama Electronic Invoicing - HKA Integration',
-    'version': '1.0.2',
+    'version': '1.0.3',
     'category': 'Accounting/Localizations',
     'summary': 'Electronic Invoicing Integration for Panama with HKA',
     'description': """
