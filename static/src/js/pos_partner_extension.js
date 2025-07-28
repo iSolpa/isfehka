54ea9da8d5bc98c96c3a4a9070d537e698d2adcf
iS-AJ <alfredo@isolpa.com>
2025-07-28 18:18:56 -0500

    odoo 18 compatibility fixes

--- static/src/js/pos_partner_extension.js
+++ static/src/js/pos_partner_extension.js
@@ -3,7 +3,7 @@
 import { patch } from "@web/core/utils/patch";
 import { PosStore } from "@point_of_sale/app/store/pos_store";
 import { PartnerListScreen } from "@point_of_sale/app/screens/partner_list/partner_list";
-import { PartnerDetailsEdit } from "@point_of_sale/app/screens/partner_list/partner_editor/partner_editor";
+import { PartnerEditor } from "@point_of_sale/app/screens/partner_editor/partner_editor";
 import { usePos } from "@point_of_sale/app/store/pos_hook";
 import { useState, onMounted, useEffect } from "@odoo/owl";
 import { _t } from "@web/core/l10n/translation";
@@ -61,7 +61,7 @@ patch(PartnerListScreen.prototype, {
     },
 });
 
-patch(PartnerDetailsEdit.prototype, {
+patch(PartnerEditor.prototype, {
     setup() {
         super.setup();
         const partner = this.props.partner;
