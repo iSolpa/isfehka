3a17afe67f76b156da89af1fac46d1db3fe2be00
iS-AJ <alfredo@isolpa.com>
2025-07-28 18:14:00 -0500

    odoo 18 compatibility fixes

--- __manifest__.py
+++ __manifest__.py
@@ -37,6 +37,11 @@
     ],
     'assets': {
         'point_of_sale._assets_pos': [
+            # Load our XML AFTER the core Partner Details template to avoid missing parent errors
+            (
+                'after',
+                'point_of_sale/static/src/xml/partner_editor.xml'
+            ),
             'isfehka/static/src/js/pos_partner_extension.js',
             'isfehka/static/src/xml/pos_partner_extension.xml',
         ],
