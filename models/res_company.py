5a6cbe230d0957ac631e3ac75571a5d6c7401562
iS-AJ <alfredo@isolpa.com>
2025-08-13 21:48:04 -0500

    support longer sucursal code length

--- models/res_company.py
+++ models/res_company.py
@@ -7,19 +7,21 @@ class ResCompany(models.Model):
     hka_branch_code = fields.Char(
         string='HKA Branch Code',
         default='0000',
-        help='Branch code for HKA integration (4 digits)'
+        size=10,
+        help='Branch code for HKA integration (up to 10 digits)'
     )
 
     hka_pos_code = fields.Char(
         string='HKA POS Code',
         default='001',
-        help='Default Point of Sale code for HKA integration (3 digits). Used when POS module is not installed.'
+        size=10,
+        help='Default Point of Sale code for HKA integration (up to 10 digits). Used when POS module is not installed.'
     )
 
     @api.constrains('hka_branch_code', 'hka_pos_code')
     def _check_hka_codes(self):
         for company in self:
-            if company.hka_branch_code and (not company.hka_branch_code.isdigit() or len(company.hka_branch_code) != 4):
-                raise ValidationError(_('HKA Branch Code must be exactly 4 digits'))
-            if company.hka_pos_code and (not company.hka_pos_code.isdigit() or len(company.hka_pos_code) != 3):
-                raise ValidationError(_('HKA POS Code must be exactly 3 digits'))
+            if company.hka_branch_code and (not company.hka_branch_code.isdigit() or len(company.hka_branch_code) < 1 or len(company.hka_branch_code) > 10):
+                raise ValidationError(_('HKA Branch Code must be between 1 and 10 digits'))
+            if company.hka_pos_code and (not company.hka_pos_code.isdigit() or len(company.hka_pos_code) < 1 or len(company.hka_pos_code) > 10):
+                raise ValidationError(_('HKA POS Code must be between 1 and 10 digits'))
