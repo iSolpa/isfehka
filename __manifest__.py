162e91b3c96bd6c33559ce7beebdc760dff5f281
iS-AJ <alfredo@isolpa.com>
2025-12-13 16:25:10 -0500

    invoice generation fixes

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Panama Electronic Invoicing - HKA Integration',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'category': 'Accounting/Localizations',
     'summary': 'Electronic Invoicing Integration for Panama with HKA',
     'description': """
--- models/__init__.py
+++ models/__init__.py
@@ -8,3 +8,4 @@ from . import pos_config
 from . import pos_session
 from . import pos_order
 from . import pos_payment_method
+from . import ir_actions_report
