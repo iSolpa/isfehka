3a7b75848539e2ae6f408c2716faa573090ea80c
iS-AJ <alfredo@isolpa.com>
2025-12-13 16:32:30 -0500

    invoice generation fixes

--- models/ir_actions_report.py
+++ models/ir_actions_report.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 from odoo import models
-from odoo.tools.misc import format_date
+from odoo.tools.misc import format_date as odoo_format_date
 
 
 class IrActionsReport(models.Model):
     _inherit = 'ir.actions.report'
 
-    def _get_rendering_context(self, report, docids, data):
+    def _render_template(self, template, values=None):
         """Ensure format_date helper is always available in QWeb report context.
         
         This fixes a KeyError: 'format_date' issue in Odoo 19 when rendering
         invoice PDFs during POS order processing where the helper was missing.
         """
-        res = super()._get_rendering_context(report, docids, data)
-        if 'format_date' not in res:
-            res['format_date'] = lambda date, date_format=False, lang_code=False: format_date(
+        if values is None:
+            values = {}
+        if 'format_date' not in values:
+            values['format_date'] = lambda date, date_format=False, lang_code=False: odoo_format_date(
                 self.env, date, date_format=date_format, lang_code=lang_code
             )
-        return res
+        return super()._render_template(template, values)
